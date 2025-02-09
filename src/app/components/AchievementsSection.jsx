8045a7f3f7ced556591804de7b8a3ccb75be5ca7
JeevanaMohanKarani <jeevanakarani06@gmail.com>
2025-02-09 19:42:23 +0530

    sf

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -12,13 +12,13 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
   {
     metric: "Projects",
-    value: "100",
+    value: "2",
     postfix: "+",
   },
   {
     prefix: "~",
     metric: "Users",
-    value: "100,000",
+    value: "12",
   },
   {
     metric: "Certified Courses",
@@ -26,7 +26,7 @@ const achievementsList = [
   },
   {
     metric: "Years",
-    value: "5",
+    value: "4",
   },
 ];
 
