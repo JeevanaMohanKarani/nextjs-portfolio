0e4b99f1b408e8648012529262674594f6a82288
JeevanaMohanKarani <jeevanakarani06@gmail.com>
2025-02-09 19:49:34 +0530

    sf

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -47,11 +47,11 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="C:\Users\acer\Downloads\my CV (4) (1).pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
+              
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+              <a href="C:\Users\acer\Downloads\my CV (4) (1).pdf"> Download CV</a>
               </span>
             </Link>
           </div>
