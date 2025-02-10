2d624cb89777e0c1db51b7a86bfa95cdd794e986
JeevanaMohanKarani <jeevanakarani06@gmail.com>
2025-02-10 18:44:50 +0530

    njhg

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,12 +22,13 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Jeevana",
+                "Judy",
                 1000,
                 "Web Developer",
                 1000,
-      
-                "DATA Analyst",
+                "Mobile Developer",
+                1000,
+                "UI/UX Designer",
                 1000,
               ]}
               wrapper="span"
@@ -36,8 +37,8 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          I'm Jeevana Karani, an aspiring developer with a strong passion for coding and technology. 
-          I am continuously expanding my skills and improving my communication to effectively share ideas and solutions.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptuous.
           </p>
           <div>
             <Link
@@ -51,7 +52,7 @@ const HeroSection = () => {
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              Download CV    
+                Download CV
               </span>
             </Link>
           </div>
